547a83806c28681e190999be3f5f29fbf4115b4a
yuna <rnssue@naver.com>
2023-12-17 20:12:44 +0900

    mission02

--- mission01/naver_login/js/main.js
+++ mission01/naver_login/js/main.js
@@ -1,70 +1,104 @@
-const user = {
-  id: "",
-  pw: "",
-};
+(function () {
+  const user = {
+    id: "asd@naver.com",
+    pw: "spdlqj123!@",
+  };
 
-/*
+  /*
+  1. email 정규표현식을 사용한 validation
+    - false면 해당 input에 is--invalid 클래스 추가     
+    - true 해당 input에 is--invalid 클래스 제거     
 
-1. email 정규표현식을 사용한 validation
-2. pw 정규표현식을 사용한 validation
-3. 상태 변수 관리
-4. 로그인 버튼을 클릭시 조건처리
+  2. pw 정규표현식을 사용한 validation
+    - false면 해당 input에 is--invalid 클래스 추가     
+    - true 해당 input에 is--invalid 클래스 제거     
 
-*/
+  3. 상태 변수 관리
 
-function emailReg(text) {
-  const re =
-    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(text).toLowerCase());
-}
 
-function pwReg(text) {
-  const re = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^*+=-]).{6,16}$/;
-  return re.test(String(text).toLowerCase());
-}
+  4. 로그인 버튼을 클릭시 조건처리
 
-const button = document.querySelector(".btn-login");
-const inputEmail = document.querySelector("#userEmail");
-const inputPassword = document.querySelector("#userPassword");
+  */
 
-function handleInputEmail() {
-  if (this.id === "userEmail") {
-    if (emailReg(this.value)) {
-      console.log("emailReg성공");
+  const emailInput = document.querySelector("#userEmail");
+  const pwInput = document.querySelector("#userPassword");
+  const loginButton = document.querySelector(".btn-login");
+
+  let emailPass = false;
+  let pwPass = false;
+
+  function emailReg(text) {
+    const re =
+      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    return re.test(String(text).toLowerCase());
+  }
+
+  function pwReg(text) {
+    const re = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^*+=-]).{6,16}$/;
+    return re.test(String(text).toLowerCase());
+  }
+
+  function handleCheckEmail() {
+    let value = this.value;
+
+    if (emailReg(value)) {
+      // success
       this.classList.remove("is--invalid");
-      user.id = this.value;
+      emailPass = true;
     } else {
-      console.log("emailReg실패");
       this.classList.add("is--invalid");
-      user.id = this.value;
+      emailPass = false;
     }
   }
-}
-function handleInputPasssword() {
-  if (this.id === "userPassword") {
-    if (pwReg(this.value)) {
-      console.log("pwReg성공");
+
+  function handleCheckPw() {
+    let value = this.value;
+
+    if (pwReg(value)) {
+      // success
       this.classList.remove("is--invalid");
-      user.pw = this.value;
+      pwPass = true;
     } else {
-      console.log("pwReg실패");
       this.classList.add("is--invalid");
-      user.pw = this.value;
+      pwPass = false;
     }
   }
-}
-inputEmail.addEventListener("input", handleInputEmail);
-inputPassword.addEventListener("input", handleInputPasssword);
 
-function href(address) {
-  window.location.href = address;
-}
+  function handleLogin(e) {
+    e.preventDefault();
 
-function handleButtonLogin(e) {
-  e.preventDefault();
+    if (emailPass && pwPass) {
+      try {
+        const id = emailInput.value;
+        const pw = pwInput.value;
+        const getUserId = user.id; // 비동기 => 1s
+        const getUserPw = user.pw; // 비동기 => 1s
 
-  if (emailReg(user.id) && pwReg(user.pw)) {
-    href("welcome.html");
+        console.log(getUserId, getUserPw);
+
+        if (id === getUserId && pw === getUserPw) {
+          // console.log('로그인 성공!');
+
+          window.location.href = "./welcome.html";
+        }
+      } catch (err) {
+        console.log("해당 유저의 정보를 가져오지 못했습니다.");
+      }
+    } else {
+      console.log("입력부터 똑바로 하고와! ");
+      // alert('dlqfurEhrqkfhgo!')
+      gsap.to("form", {
+        y: 10,
+        repeat: 8,
+        yoyo: true,
+        duration: 0.08,
+        clearProps: true,
+      });
+    }
   }
-}
-button.addEventListener("click", handleButtonLogin);
+
+  emailInput.addEventListener("input", handleCheckEmail);
+  pwInput.addEventListener("input", handleCheckPw);
+  loginButton.addEventListener("click", handleLogin);
+})();
